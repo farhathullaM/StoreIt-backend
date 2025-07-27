c92e12ffa5c2236c68b7aead94dd00146a0e162e
farhathulla <farhathulla01@gmail.com>
2025-07-27 13:28:12 +0530

    frontend path add

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://storeitgo-kkkkks-projects-113c40d3.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
