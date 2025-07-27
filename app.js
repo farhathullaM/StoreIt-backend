a529d76f23a071c7a5a9869214f4701772aa53f6
farhathulla <farhathulla01@gmail.com>
2025-07-27 13:48:06 +0530

    vercel hosted path

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://storeitgo-kkkkks-projects-113c40d3.vercel.app",
+      "https://store-it-six-theta.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
